9789af9e3eb6f1f520f059cda29d43990ca93934
RomualdoFranca <romualdo.ads2022@gmail.com>
2022-09-25 21:12:37 -0300

    error schema endpoint

--- src/main/java/org/example/RestApi.java
+++ src/main/java/org/example/RestApi.java
@@ -1,8 +1,32 @@
 package org.example;
 
-public class RestApi {
-    public static void main(String[] args) {
+import com.google.gson.Gson;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class RestApi {
+    public static void main(String[] args) throws Exception {
+
+        Transcript transcript = new Transcript();
+        transcript.setAudioURL("https://raw.githubusercontent.com/johnmarty3/JavaAPITutorial/main/Thirsty.mp4");
+        Gson gson = new Gson();
+        String jsonRequest = gson.toJson(transcript);
+        System.out.println(jsonRequest);
+
+        HttpRequest postRequest = HttpRequest.newBuilder()
+                .uri(new URI("https://api.assemblyai.com/v2/transcript"))
+                .header("Authorization", "51b7de0d8d1249f2beb6de2b092f0f32")
+                .POST(HttpRequest.BodyPublishers.ofString(jsonRequest))
+                .build();
+
+        HttpClient httpClient = HttpClient.newHttpClient();
+
+        HttpResponse<String> postResponse =  httpClient.send(postRequest, HttpResponse.BodyHandlers.ofString());
+//        postResponse.body();
+        System.out.println(postResponse.body());
 
     }
 }
---
+++ src/main/java/org/example/Transcript.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Transcript {
+    private  String audioURL;
+
+    public String getAudioURL() {
+        return audioURL;
+    }
+
+    public void setAudioURL(String audioURL) {
+        this.audioURL = audioURL;
+    }
+}
